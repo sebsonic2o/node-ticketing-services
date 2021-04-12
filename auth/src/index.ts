8d5a1124a170e87a3adcfa40e32631f09e4c2f3c
Sebastien Cheraux <scheraux@gmail.com>
2021-04-11 18:22:45 -0700

    set up auth service boiler plate

---
+++ auth/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { json } from 'body-parser';
+import { currentUserRouter } from './routes/currentuser';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { signUpRouter } from './routes/signup';
+
+const app = express();
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.listen(3000, () => {
+  console.log('listening on port 3000...');
+});
