197834a9c8552048ce0da73b7da15e41ab2ff905
Sebastien Cheraux <scheraux@gmail.com>
2021-04-14 19:49:32 -0700

    leverage package to abstract away complexity of error handling for async requests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/currentuser';
@@ -17,7 +18,7 @@ app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
 
-app.all('*', () => {
+app.all('*', async () => {
   throw new NotFoundError();
 });
 
