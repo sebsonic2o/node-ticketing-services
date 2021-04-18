e54d960b86931331b0a5b9ce0d442bddf651f3e7
Sebastien Cheraux <scheraux@gmail.com>
2021-04-17 17:46:00 -0700

    format (user) json properties to strive for common response from different services

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -26,7 +26,16 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true
-  },
+  }
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.password;
+      delete ret.__v;
+    }
+  }
 });
 
 // a pre-save hook to hash password
