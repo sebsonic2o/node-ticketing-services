643fdcd16be0096c8dade57a9d56c9339e18d10b
Sebastien Cheraux <scheraux@gmail.com>
2021-04-18 18:34:16 -0700

    add test on signout

---
+++ auth/src/routes/__test__/signout.test.ts
@@ -0,0 +1,21 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns 200 and clears cookie on successful signout', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'valid@email.com',
+      password: '12345678'
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post('/api/users/signout')
+    .send({})
+    .expect(200);
+
+  expect(response.get('Set-Cookie')[0]).toEqual(
+    'express:sess=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly'
+  );
+});
