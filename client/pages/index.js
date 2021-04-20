1afc2126d200a86064bf0511b406ac81e75cf29f
Sebastien Cheraux <scheraux@gmail.com>
2021-04-19 17:26:22 -0700

    work around (client) requests from server rather than browser with server-side rendering

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,30 @@
-const Home = () => {
-  return <h1>Landing Page</h1>;
+import axios from 'axios';
+
+const Home = ({ currentUser }) => {
+  console.log(currentUser);
+
+  return (
+    <h1>Landing Page</h1>
+  );
+};
+
+Home.getInitialProps = async ({ req }) => {
+  let data = {};
+
+  if (typeof window === 'undefined') {
+    // from server
+    ({ data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers
+      }
+    ));
+  } else {
+    // from browser
+    ({ data } = await axios.get('/api/users/currentuser'));
+  }
+
+  return data;
 };
 
 export default Home;
