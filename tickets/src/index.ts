bfebd2f571681712ebe50347e538dd8cb072039f
Sebastien Cheraux <scheraux@gmail.com>
2021-04-30 15:37:19 -0700

    add graceful shutdown

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -13,6 +13,12 @@ const start = async () => {
 
   try {
     await natsWrapper.connect('ticketing', '1234', 'http://nats-str-srv:4222');
+    natsWrapper.client.on('close', () => {
+      console.log('nats connection closed...');
+      process.exit();
+    });
+    process.on('SIGINT', () => { natsWrapper.client.close() });
+    process.on('SIGTERM', () => { natsWrapper.client.close() });
 
     console.log('connecting to mongo db...');
     await mongoose.connect(process.env.MONGO_URI, {
