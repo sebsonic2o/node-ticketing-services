cc39ae634a0d5b390bcd34780efa4b155a313313
Sebastien Cheraux <scheraux@gmail.com>
2021-04-29 17:32:53 -0700

    leverage typescript to validate listeners

--- nats-str-test/src/events/ticket-created-listener.ts
+++ nats-str-test/src/events/ticket-created-listener.ts
@@ -1,11 +1,13 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subject } from './subject';
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subject.TicketCreated;
   queueGroupName = 'listener-service';
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('with event data', data);
 
     msg.ack();
