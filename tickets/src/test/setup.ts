f2509ec6359fda92539042fd9eb4775cf1800885
Sebastien Cheraux <scheraux@gmail.com>
2021-05-01 12:26:11 -0700

    remove extraneous imports

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,8 +1,6 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import request from 'supertest';
 import jwt from 'jsonwebtoken';
-import { app } from '../app';
 
 declare global {
   namespace NodeJS {
