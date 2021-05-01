a916a442c345c7242cf80ea6096f8e26dd7f8010
Sebastien Cheraux <scheraux@gmail.com>
2021-05-01 13:29:50 -0700

    implement order index route (test driven)

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,72 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+const path = '/api/orders';
+
+const createTicket = async () => {
+  const ticket = Ticket.build({
+    title: 'title',
+    price: 100
+  });
+
+  await ticket.save();
+
+  return ticket;
+}
+
+it('cannot be accessed when user is not signed in', async () => {
+  await request(app)
+    .get(path)
+    .send()
+    .expect(401);
+});
+
+it('can be accessed when user is signed in', async () => {
+  const response = await request(app)
+    .get(path)
+    .set('Cookie', global.signin())
+    .send();
+
+  expect(response.status).not.toEqual(401);
+});
+
+it('returns list of orders for given user', async () => {
+  const ticketOne = await createTicket();
+  const ticketTwo = await createTicket();
+  const ticketThree = await createTicket();
+
+  const userOne = global.signin();
+  const userTwo = global.signin();
+
+  await request(app)
+    .post(path)
+    .set('Cookie', userOne)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+
+  const { body: orderOne } = await request(app)
+    .post(path)
+    .set('Cookie', userTwo)
+    .send({ ticketId: ticketTwo.id })
+    .expect(201);
+
+  const { body: orderTwo } = await request(app)
+    .post(path)
+    .set('Cookie', userTwo)
+    .send({ ticketId: ticketThree.id })
+    .expect(201);
+
+  const response = await request(app)
+    .get(path)
+    .set('Cookie', userTwo)
+    .send();
+
+  expect(response.status).toEqual(200);
+  expect(response.body.length).toEqual(2);
+  expect(response.body[0].id).toEqual(orderOne.id);
+  expect(response.body[0].ticket.id).toEqual(ticketTwo.id);
+  expect(response.body[1].id).toEqual(orderTwo.id);
+  expect(response.body[1].ticket.id).toEqual(ticketThree.id);
+});
--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response} from 'express';
+import { requireAuth } from '@sebsonic2o-org/common';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
 router.get(
   '/api/orders',
+  requireAuth,
   async (req: Request, res: Response) => {
-    res.send({});
+    const orders = await Order.find({
+      userId: req.currentUser!.id
+    }).populate('ticket');
+
+    res.send(orders);
 });
 
 export { router as indexOrderRouter };
