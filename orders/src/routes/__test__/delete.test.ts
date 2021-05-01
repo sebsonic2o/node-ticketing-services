326575598e422d2d85d48d40ac5d5aad7bb1fb63
Sebastien Cheraux <scheraux@gmail.com>
2021-05-01 15:14:00 -0700

    implement order delete/cancel route (test driven)

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,82 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { OrderStatus } from '../../models/order';
+
+const path = '/api/orders';
+
+it('cannot be accessed when user is not signed in', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .delete(`${path}/${id}`)
+    .send()
+    .expect(401);
+});
+
+it('returns error if order is not found', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .delete(`${path}/${id}`)
+    .set('Cookie', global.signin())
+    .send()
+    .expect(404);
+});
+
+it('returns error when user does not own order', async () => {
+  const ticket = Ticket.build({
+    title: 'title',
+    price: 100
+  });
+  await ticket.save();
+
+  const { body: order } = await request(app)
+    .post(path)
+    .set('Cookie', global.signin())
+    .send({
+      ticketId: ticket.id
+    })
+    .expect(201);
+
+  await request(app)
+    .delete(`${path}/${order.id}`)
+    .set('Cookie', global.signin())
+    .send()
+    .expect(401);
+});
+
+it('cancels order for given user', async () => {
+  const ticket = Ticket.build({
+    title: 'title',
+    price: 100
+  });
+  await ticket.save();
+
+  const cookie = global.signin();
+
+  const { body: order } = await request(app)
+    .post(path)
+    .set('Cookie', cookie)
+    .send({
+      ticketId: ticket.id
+    })
+    .expect(201);
+
+  await request(app)
+    .delete(`${path}/${order.id}`)
+    .set('Cookie', cookie)
+    .send()
+    .expect(204);
+
+  const response = await request(app)
+    .get(`${path}/${order.id}`)
+    .set('Cookie', cookie)
+    .send()
+    .expect(200);
+
+  expect(response.body.status).toEqual(OrderStatus.Cancelled);
+});
+
+it.todo('publishes an order cancelled event');
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,11 +1,32 @@
 import express, { Request, Response} from 'express';
+import { requireAuth, NotFoundError, UnauthorizedError } from '@sebsonic2o-org/common';
+import { Order, OrderStatus } from '../models/order';
 
 const router = express.Router();
 
 router.delete(
   '/api/orders/:id',
+  requireAuth,
   async (req: Request, res: Response) => {
-    res.send({});
+    const order = await Order.findById(req.params.id);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new UnauthorizedError();
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled
+    });
+
+    await order.save();
+
+    // publish order cancelled event
+
+    res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter };
