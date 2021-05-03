07b35efd96a46e6bb4f7f62f9b7812b05ff7c3a6
Sebastien Cheraux <scheraux@gmail.com>
2021-05-03 09:44:37 -0700

    implement order cancelled listener

---
+++ tickets/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { OrderCancelledEvent } from '@sebsonic2o-org/common';
+import { OrderCancelledListener } from '../order-cancelled-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Ticket } from '../../../models/ticket';
+
+const setup = async () => {
+  // create listener instance
+  const listener = new OrderCancelledListener(natsWrapper.client);
+
+  // create ticket
+  const orderId = new mongoose.Types.ObjectId().toHexString();
+  const ticket = Ticket.build({
+    title: 'title',
+    price: 100,
+    userId: new mongoose.Types.ObjectId().toHexString()
+  });
+  ticket.set({ orderId });
+  await ticket.save();
+
+  // create fake data
+  const data: OrderCancelledEvent['data'] = {
+    id: orderId,
+    version: 1,
+    ticket: {
+      id: ticket.id
+    }
+  };
+
+  // create fake message
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn()
+  };
+
+  return { listener, data, msg };
+};
+
+it('releases ticket by unsetting order id', async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const ticket = await Ticket.findById(data.ticket.id);
+
+  expect(ticket!.orderId).toBeUndefined();
+});
+
+it('publishes ticket updated event', async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalledWith('ticket:updated', expect.anything(), expect.anything());
+});
+
+it('acknowledges message', async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
--- tickets/src/events/listeners/order-cancelled-listener.ts
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -1,12 +1,32 @@
 import { Message } from 'node-nats-streaming';
 import { Listener, Subject, OrderCancelledEvent } from '@sebsonic2o-org/common';
 import { queueGroupName } from './queue-group-name';
+import { Ticket } from '../../models/ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 
 export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
   readonly subject = Subject.OrderCancelled;
   readonly queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error('ticket not found');
+    }
+
+    ticket.set({ orderId: undefined });
+    await ticket.save();
+
+    new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      orderId: ticket.orderId,
+      userId: ticket.userId
+    });
+
     msg.ack();
   }
 }
