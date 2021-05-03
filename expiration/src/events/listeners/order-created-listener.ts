cac0d0250675d8a7340daf8bfc2beb65271955f0
Sebastien Cheraux <scheraux@gmail.com>
2021-05-03 16:56:46 -0700

    add job to queue on order cancelled event

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,12 +1,17 @@
 import { Message } from 'node-nats-streaming';
 import { Listener, Subject, OrderCreatedEvent } from '@sebsonic2o-org/common';
 import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subject.OrderCreated;
   readonly queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id
+    });
+
     msg.ack();
   }
 }
