b37782da6f22e3fdece0b98040e906846ed5c8f7
Sebastien Cheraux <scheraux@gmail.com>
2021-05-03 18:42:49 -0700

    add expiration complete listener for orders service

---
+++ orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -0,0 +1,68 @@
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { ExpirationCompleteEvent, OrderStatus } from '@sebsonic2o-org/common';
+import { ExpirationCompleteListener } from '../expiration-complete-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Ticket } from '../../../models/ticket';
+import { Order } from '../../../models/order';
+
+const setup = async () => {
+  // create listener instance
+  const listener = new ExpirationCompleteListener(natsWrapper.client);
+
+  // create order
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: 'title',
+    price: 100
+  });
+  await ticket.save();
+
+  const order = Order.build({
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    expiresAt: new Date(),
+    ticket
+  });
+  await order.save();
+
+  // create fake data
+  const data: ExpirationCompleteEvent['data'] = {
+    orderId: order.id
+  };
+
+  // create fake message
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn()
+  };
+
+  return { listener, data, msg };
+};
+
+it('cancels order', async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const order = await Order.findById(data.orderId);
+
+  expect(order!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('publishes order cancelled event', async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalledWith('order:cancelled', expect.anything(), expect.anything());
+});
+
+it('acknowledges message', async () => {
+  const { listener, data, msg } = await setup();
+
+  // call onmessage with data and message
+  await listener.onMessage(data, msg);
+
+  // assert ack function call
+  expect(msg.ack).toHaveBeenCalled();
+});
