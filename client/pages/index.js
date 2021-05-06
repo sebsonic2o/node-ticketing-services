764d59f1d991eb19682b9695d5a5024e2bf6ceae
Sebastien Cheraux <scheraux@gmail.com>
2021-05-06 08:54:27 -0700

    fix current user double fetching

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,9 @@
-import buildClient from '../api/build-client';
-
 const Home = ({ currentUser }) => {
   return <h1>{currentUser ? 'You are signed in' : 'You are NOT signed in'}</h1>;
 };
 
 Home.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser');
-
-  return data;
+  return {};
 };
 
 export default Home;
