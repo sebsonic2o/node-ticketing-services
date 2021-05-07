975bf1fd592bbbf667985ed01d2c5561f0b8379f
Sebastien Cheraux <scheraux@gmail.com>
2021-05-07 11:41:40 -0700

    show ticket and create order (client)

--- client/pages/tickets/[id].js
+++ client/pages/tickets/[id].js
@@ -1,7 +1,32 @@
-const ShowTicket = () => {
+import useRequest from '../../hooks/use-request';
+
+const ShowTicket = ({ ticket }) => {
+  const { doRequest, errors } = useRequest({
+    url: '/api/orders',
+    method: 'post',
+    body: {
+      ticketId: ticket.id
+    },
+    onSuccess: (order) => {
+      console.log(order);
+    }
+  });
+
   return (
-    <h1>Ticket</h1>
+    <div>
+      <h1>{ticket.title}</h1>
+      <h4>Price: {ticket.price}</h4>
+      {errors}
+      <button onClick={doRequest} className="btn btn-primary">Purchase</button>
+    </div>
   );
+};
+
+ShowTicket.getInitialProps = async (context, client) => {
+  const { id } = context.query;
+  const { data } = await client.get(`/api/tickets/${id}`);
+
+  return { ticket: data };
 };
 
 export default ShowTicket;
