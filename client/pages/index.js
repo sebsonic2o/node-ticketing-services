c3becece5bf778dc9c88ead2535a83a95938b0ae
Sebastien Cheraux <scheraux@gmail.com>
2021-05-06 17:50:09 -0700

    list tickets (client)

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,35 @@
-const Home = ({ currentUser }) => {
-  return <h1>{currentUser ? 'You are signed in' : 'You are NOT signed in'}</h1>;
+const Home = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ticketList}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 Home.getInitialProps = async (context, client, currentUser) => {
-  return {};
+  const { data } = await client.get('/api/tickets');
+
+  return { tickets: data };
 };
 
 export default Home;
