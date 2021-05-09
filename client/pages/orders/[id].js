a816e11477a7276674dac4a9e6ac70b71ea690a2
Sebastien Cheraux <scheraux@gmail.com>
2021-05-08 19:24:25 -0700

    add expiration timer (client)

--- client/pages/orders/[id].js
+++ client/pages/orders/[id].js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react';
+
 const ShowOrder = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
+  if (timeLeft < 0) {
+    return <h4>Your time has expired</h4>;
+  }
+
   return (
     <div>
       <h1>Purchasing {order.ticket.title}</h1>
+      <h4>You have {timeLeft} seconds left to pay</h4>
       <button className="btn btn-primary">Pay</button>
     </div>
   );
